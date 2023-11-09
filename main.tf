b62ce0ee5bd635581d82140e1b51ebcd1a3dcd30
pappik <pappik@gmail.com>
2023-11-08 20:42:55 -0500

    parameters store testing

--- main.tf
+++ main.tf
@@ -2,3 +2,11 @@ resource "aws_vpc" "main" {
   cidr_block = var.cidr_block
   tags = merge(local.common_tags, { Name = "${var.env}-vpc" } )
 }
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = cidr_block
+
+  tags = merge(local.common_tags, { Name = "${var.env}-subnet" } )
+
+}
