a61d81f0c253e64d9154bed9c8510d23f377a514
pappik <pappik@gmail.com>
2023-11-09 14:26:47 -0500

    nat gateway

--- main.tf
+++ main.tf
@@ -60,6 +60,22 @@ resource "aws_route_table_association" "public-rt-assoc" {
   route_table_id = aws_route_table.public.id
 }
 
+resource "aws_eip" "ngw-eip" {
+  domain   = "vpc"
+}
+
+resource "aws_nat_gateway" "ngw" {
+  allocation_id = aws_eip.ngw-eip.id
+  subnet_id     = aws_subnet.public.*.id[0]
+
+  tags = merge(local.common_tags, { Name = "${var.env}-ngw" } )
+
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+#  depends_on = [aws_internet_gateway.example]
+}
+
 ###Ec2
 
 #data "aws_ami" "test_instance" {
