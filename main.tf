de6570c7707c7ee37cd986589406746c86be06a7
pappik <pappik@gmail.com>
2023-11-09 16:50:43 -0500

    availabilty zones addition

--- main.tf
+++ main.tf
@@ -7,6 +7,7 @@ resource "aws_subnet" "public" {
   count      = length(var.public_subnets_cidr)
   vpc_id     = aws_vpc.main.id
   cidr_block = var.public_subnets_cidr[count.index]
+  availability_zone = var.availability_zones[count.index]
 
   tags = merge(local.common_tags, { Name = "${var.env}-public-subnet-${count.index+1}" } )
 
@@ -16,6 +17,8 @@ resource "aws_subnet" "private" {
   count      = length(var.private_subnets_cidr)
   vpc_id     = aws_vpc.main.id
   cidr_block = var.private_subnets_cidr[count.index]
+  availability_zone = var.availability_zones[count.index]
+
 
   tags = merge(local.common_tags, { Name = "${var.env}-private-subnet-${count.index+1}" } )
 
