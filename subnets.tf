b2630d8078b30238f5bad068365a828b6136c260
pappik <pappik@gmail.com>
2023-11-13 20:18:53 -0500

    global code for isolated resources

--- subnets.tf
+++ subnets.tf
@@ -11,7 +11,7 @@ module "subnets" {
 
   vpc_id                    = aws_vpc.main.id
   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
-  tags                      = local.common_tags
+
 }
 
 #resource "aws_route" "r" {
