94d4a0dd872a09d983646966ac017f221dfd71b6
pappik <pappik@gmail.com>
2023-11-13 20:40:42 -0500

    global code for isolated resources

--- subnets/locals.tf
+++ subnets/locals.tf
@@ -1,15 +1,3 @@
 locals {
   common_tags = var.tags
 }
-
-
-
-locals {
-  common_tags = {
-    env = var.env
-    project = "robo shop"
-    business_unit = "e-commerce"
-    owner = "ecommerce-robot"
-
-  }
-}
